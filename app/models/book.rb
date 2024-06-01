2da659e16e57bd4e99d4549991b602295861979e
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-06-01 09:39:39 +0000

    iine

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true
@@ -13,4 +15,9 @@ class Book < ApplicationRecord
       'noimage.jpg'
     end
   end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'favorites/create'
+  get 'favorites/destroy'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   root to: "home#top"
 
   get "/home/about" => "home#about", as: "about"
-  resources :books, only: [:new, :index, :show, :create, :destroy, :edit, :update]
+  
+  resources :books, only: [:new, :index, :show, :create, :destroy, :edit, :update] do
+    resource :favorite, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end 
+  
   resources :users, only: [:show, :edit, :update, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
