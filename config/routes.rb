e8e1bb85c2a48f9c70e2c80bdf5086abc937ebb9
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-06-01 12:48:40 +0000

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'relationships/followings'
+  get 'relationships/followers'
   get 'favorites/create'
   get 'favorites/destroy'
   devise_for :users, controllers: {
@@ -14,7 +16,11 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end 
   
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :users, only: [:show, :edit, :update, :index] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
