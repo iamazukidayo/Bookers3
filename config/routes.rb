b7cd2b9f767a42c158f95978aab4b0690ac893c5
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-06-07 10:15:28 +0000

    8c

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,11 @@ Rails.application.routes.draw do
   
   resources :messages, only: [:create]
   resources :rooms, only: [:create, :show]
-  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
+ 
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
+    resource :group_users, only: [:create, :destroy]
+  end 
+ 
   get "search" => "searches#search"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
