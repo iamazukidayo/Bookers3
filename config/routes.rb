2e4dfad9eb3c14cc3147b76de0c7fb8711c13a4f
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-06-07 13:18:55 +0000

    9c-1

--- config/routes.rb
+++ config/routes.rb
@@ -10,31 +10,34 @@ Rails.application.routes.draw do
   root to: "home#top"
 
   get "/home/about" => "home#about", as: "about"
-  
+
   resources :books, only: [:new, :index, :show, :create, :destroy, :edit, :update] do
     resource :favorite, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
-  end 
-  
+  end
+
   resources :users, only: [:show, :edit, :update, :index] do
      member do
        get :follows, :followers
-     end 
+     end
     get "posts_on_date" => "users#posts_on_date"
-     
+
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
+
   resources :messages, only: [:create]
   resources :rooms, only: [:create, :show]
- 
+
   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
     resource :group_users, only: [:create, :destroy]
-  end 
- 
+     get "new/mail" => "groups#new_mail"
+     get "send/mail" => "groups#send_mail"
+   end
+
   get "search" => "searches#search"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
